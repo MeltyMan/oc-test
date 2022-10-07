365aeac78d3e865a14ae0996f3af4dbf8c5cbfe8
MeltyMan <kalle.johansson@hiq.se>
2022-10-07 11:37:37 +0200

    First Commit

---
+++ octestapi/Controllers/KalleController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace octestapi.Controllers;
+[ApiController]
+[Route("[controller]")]
+
+public class KalleController : ControllerBase
+{
+    [HttpGet]
+    [Route("hello")]
+    public async Task<ActionResult<string>> Hello([FromQuery] string name)
+    {
+        var result = "Hello";
+        if (!string.IsNullOrEmpty(name))
+        {
+            result = $"{result} {name}";
+        }
+
+        result = $"{result}!";
+        
+        return Ok(result);
+    }
+}
