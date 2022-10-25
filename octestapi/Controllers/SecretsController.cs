976311b7aae600ac65927d1a0bae9bf77e48428f
MeltyMan <karl.johansson@sigmatechnology.se>
2022-10-25 14:16:17 +0200

    Added secrets

---
+++ octestapi/Controllers/SecretsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace octestapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("{path}")]
+        public ActionResult<string> GetSecret([FromRoute] string path)
+        {
+            var result = _configuration.GetValue<string>(path);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
